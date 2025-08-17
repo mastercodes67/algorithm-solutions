23639aa1682f1c23205a34124b178b409b1c1597
Batuhan Akpınar <powerbatu12@gmail.com>
2025-08-17 14:30:28 +0300

    Add two algorithms: letter grade distribution and max grade

---
+++ letter_grade_algorithm/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        //Harf notu hesaplayacaz
+        string[] grades = new string[7] { "A", "B", "C", "D", "F", "C", "B" };
+        int[] statics = new int[5];
+        for (int i = 0; i < grades.Length; i++)
+        {
+            if (grades[i] == "A")
+            {
+                statics[0]++;
+            }
+            else if (grades[i] == "B")
+            {
+                statics[1]++;
+            }
+            else if (grades[i] == "C")
+            {
+                statics[2]++;
+            }
+            else
+            {
+                statics[3]++;
+            }
+            Console.WriteLine("Öğrencilerin Sıraysıyla Harf notu Dağılımı");
+
+        }
+          for (int i=0; i < 5; i++) {
+                 Console.WriteLine(statics[i]);
+            }
+        
+    }
+}
